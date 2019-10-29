69705b4a1c4ab60cd3f38115a1bc9bb0910963b4
RaimundoDiaz <53487052+RaimundoDiaz@users.noreply.github.com>
2019-10-29 12:29:58 -0300

    Bug fix

---
+++ vle_content_file/WindowsFormsApp1/AgregarEquipoEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using ModelosRally;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistasRally
+{
+    public class AgregarEquipoEventArgs : EventArgs
+    {
+        public string nombre { get; set; }
+        public Vehiculo vehiculo { get; set; }
+        public Persona piloto { get; set; }
+        public Persona navegante { get; set; }
+    }
+}
